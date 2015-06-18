2b28905b8bb442e4d92fc4a70bd498df17a98062
ffazil <firoz.fazil@gmail.com>
2015-06-18 16:33:48 +0530

    Initial commit.

---
+++ src/main/java/com/tracebucket/x1/notification/domain/Email.java
@@ -0,0 +1,8 @@
+package com.tracebucket.x1.notification.domain;
+
+/**
+ * @author ffazil
+ * @since 18/06/15
+ */
+public class Email {
+}
---
+++ src/main/java/com/tracebucket/x1/notification/domain/NotificationType.java
@@ -0,0 +1,12 @@
+package com.tracebucket.x1.notification.domain;
+
+/**
+ * @author ffazil
+ * @since 18/06/15
+ */
+public enum NotificationType {
+    EMAIL,
+    TWEET,
+    WALL_POST,
+    SMS
+}
